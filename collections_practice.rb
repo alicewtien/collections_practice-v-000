1c87357b358f525e4a0dad75f86004db16efffbe
alice T <alicewtien@gmail.com>
2018-09-08 15:39:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,11 @@
+#question 1 - ascending order
 def sort_array_asc(numbers)
   numbers.sort
 end
+
+#question 2 - descending order
+def sort_array_desc(numbers)
+  numbers.sort { |a, b| b <=> a }
+end
+
+#question 3
